4f95ed996a58d960b70152d4bb4e722eeef30dbd
martinssvinicius <viniscmartins@gmail.com>
2025-05-04 15:45:18 -0300

    update

---
+++ frontend/src/app/components/pages/home/home.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Food } from '../../../shared/models/Food';
+import { FoodService } from '../../../services/food.service';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: false,
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit {
+
+  foods: Food[] = [];
+  
+  constructor(private foodService: FoodService, activatedRoute: ActivatedRoute) {
+    activatedRoute.params.subscribe((params) => {
+      if (params.searchTerm) {
+        this.foods = this.foodService.getAllFoodsBySearchTerm(params.searchTerm);
+      } else {
+        this.foods = foodService.getAll();
+      }
+    });
+  }
+
+  ngOnInit(): void {
+    
+  }
+
+}
